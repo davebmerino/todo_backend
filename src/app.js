601561ec7650005b822ccec0bd41622efd99de60
davebmerino <daveb.merino@gmail.com>
2026-09-04 00:07:55 +0800

    cors

--- src/app.js
+++ src/app.js
@@ -14,8 +14,12 @@ const authRouter = require("./auth/auth.routes.js");
 const app = express();
 
 app.use(express.json());
-app.use(cors({}));
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 //access.log file
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "..", "access.log"),
