bcc3b38e6c81db5108dc4b1916eac16687234b3b
davebmerino <daveb.merino@gmail.com>
2026-09-13 10:56:08 +0800

    cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "https://todo-frontend-phi-sandy.vercel.app/",
+      "https://todo-frontend-phi-sandy.vercel.app",
       "http://localhost:5173/",
     ],
     credentials: true,
